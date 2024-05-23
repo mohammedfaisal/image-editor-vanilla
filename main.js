ad47e878e85990685f64c9f7860494492a22ca8a
Mohammed Faisal <faisal>
2024-05-23 17:19:17 +0530

    Modified VanillaJs example provided in the github documentation to use only ADJUST tab of the image-editor

--- main.js
+++ main.js
@@ -1,70 +1,18 @@
 import './style.css';
-import FilerobotImageEditor from 'filerobot-image-editor'; // Load library from NPM
-// or load from CDN as following and use (window.FilerobotImageEditor):
-// <script src="https://scaleflex.cloudimg.io/v7/plugins/filerobot-image-editor/latest/filerobot-image-editor.min.js"></script>
+import FilerobotImageEditor from 'filerobot-image-editor'; 
 
 const { TABS, TOOLS } = FilerobotImageEditor;
 const config = {
   source: 'https://scaleflex.airstore.io/demo/stephen-walker-unsplash.jpg',
-  onSave: (editedImageObject, designState) =>
-    console.log('saved', editedImageObject, designState),
-  annotationsCommon: {
-    fill: '#ff0000',
-  },
-  Text: { text: 'Filerobot...' },
-  Rotate: { angle: 90, componentType: 'slider' },
-  translations: {
-    profile: 'Profile',
-    coverPhoto: 'Cover photo',
-    facebook: 'Facebook',
-    socialMedia: 'Social Media',
-    fbProfileSize: '180x180px',
-    fbCoverPhotoSize: '820x312px',
-  },
+  tabsIds: [TABS.ADJUST],
+  defaultTabId: TABS.ADJUST,
+  defaultToolId: TOOLS.CROP,
+  defaultSavedImageQuality: 1.0,
   Crop: {
-    presetsItems: [
-      {
-        titleKey: 'classicTv',
-        descriptionKey: '4:3',
-        ratio: 4 / 3,
-        // icon: CropClassicTv, // optional, CropClassicTv is a React Function component. Possible (React Function component, string or HTML Element)
-      },
-      {
-        titleKey: 'cinemascope',
-        descriptionKey: '21:9',
-        ratio: 21 / 9,
-        // icon: CropCinemaScope, // optional, CropCinemaScope is a React Function component.  Possible (React Function component, string or HTML Element)
-      },
-    ],
-    presetsFolders: [
-      {
-        titleKey: 'socialMedia', // will be translated into Social Media as backend contains this translation key
-        // icon: Social, // optional, Social is a React Function component. Possible (React Function component, string or HTML Element)
-        groups: [
-          {
-            titleKey: 'facebook',
-            items: [
-              {
-                titleKey: 'profile',
-                width: 180,
-                height: 180,
-                descriptionKey: 'fbProfileSize',
-              },
-              {
-                titleKey: 'coverPhoto',
-                width: 820,
-                height: 312,
-                descriptionKey: 'fbCoverPhotoSize',
-              },
-            ],
-          },
-        ],
-      },
-    ],
+      noPresets: true,
   },
-  tabsIds: [TABS.ADJUST, TABS.ANNOTATE, TABS.WATERMARK], // or ['Adjust', 'Annotate', 'Watermark']
-  defaultTabId: TABS.ANNOTATE, // or 'Annotate'
-  defaultToolId: TOOLS.TEXT, // or 'Text'
+  removeSaveButton: true,
+  onSave: (editedImageObject, designState) => console.log('Save invoked', editedImageObject, designState),
 };
 
 // Assuming we have a div with id="editor_container"
